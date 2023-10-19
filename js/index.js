e61e9b6b16cb6d50a66f0cf14171f569294f4191
Vitor Domingos <vm486092@gmail.com>
2023-10-19 15:22:12 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+// Laço de repetição while é o mais utilizado
+
+// Definindo duas variáveis de senha
+var primeiraSenha = 39522
+var segundaSenha = 39497
+
+// Enquanto segundaSenha for menor ou igual a primeiraSenha, execute o seguinte bloco de código
+while(segundaSenha <= primeiraSenha){
+    console.log("Incrementando a variável com nome segundaSenha" + segundaSenha)
+    segundaSenha++ // Incrementa o valor de segundaSenha a cada iteração
+}
+
+// Definindo duas variáveis numéricas
+let primeiroValorNumerico = 0.40
+let segundoValorNumerico = 0.20
+
+// Faça o seguinte bloco de código pelo menos uma vez e enquanto segundoValorNumerico for menor ou igual a primeiroValorNumerico, continue fazendo-o
+do {
+    console.log("Segundo valor numérico" + segundoValorNumerico)
+    segundoValorNumerico++ // Incrementa o valor de segundoValorNumerico a cada iteração
+} while (segundoValorNumerico <= primeiroValorNumerico)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
